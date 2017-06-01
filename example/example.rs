639bfce44608edbd0d73181ac5ecc6cd294854b4
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-06-01 15:04:28 -0700

    First commit

---
+++ example/example.rs
@@ -0,0 +1,49 @@
+extern crate liar;
+
+use liar::{black_box, Bencher};
+
+
+mod acker {
+    pub fn mann(m: usize, n: usize) -> usize {
+        match m {
+            0 => n + 1,
+            _ => match n {
+                0 => mann(m - 1, 1),
+                n => mann(m - 1, mann(m, n - 1)),
+            }
+        }
+    }
+
+}
+
+fn nop(b: &mut Bencher) {
+    b.run(|| {});
+}
+
+fn nop_black_box(b: &mut Bencher) {
+    b.run(|| (black_box(3), black_box(2)));
+}
+
+fn ack(b: &mut Bencher) {
+    b.run(|| {
+        acker::mann(3, 2)
+    });
+}
+
+fn ack_black_box(b: &mut Bencher) {
+    b.run(|| {
+        acker::mann(black_box(3), black_box(2))
+    });
+}
+
+fn main() {
+    let mut b = liar::Bencher::new();
+
+    b.bench("nop", &mut nop);
+    b.bench("nop_black_box", &mut nop_black_box);
+    b.bench("ack", &mut ack);
+    b.bench("ack_black_box", &mut ack_black_box);
+
+    let r = liar::Reporter::new();
+    r.report(&b.samples());
+}
---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+#![feature(test)]
+extern crate test;
+
+use std::time::{Instant, Duration};
+
+pub fn black_box<T>(x: T) -> T {
+    test::black_box(x)
+}
+
+pub struct Sample {
+    name: &'static str,
+    dur: Duration,
+    count: u32,
+}
+
+pub struct Bencher {
+    name: Option<&'static str>,
+    samples: Vec<Sample>,
+}
+
+impl Bencher {
+    pub fn new() -> Self {
+        Bencher {
+            name: None,
+            samples: vec![],
+        }
+    }
+
+    pub fn run<Target, Ret>(&mut self, mut target: Target)
+        where Target: FnMut() -> Ret {
+        let count = 100_000_u32;
+
+        let now = Instant::now();
+        for _ in 0..count {
+            black_box(target());
+        }
+        let dur = now.elapsed();
+
+        self.samples.push(Sample {
+            name: self.name.unwrap(),
+            dur,
+            count,
+        });
+    }
+
+    pub fn bench<T>(&mut self, name: &'static str, target: &mut T)
+        where T: FnMut(&mut Bencher) {
+        self.name = Some(name);
+        target(self);
+    }
+
+    pub fn samples(&self) -> &Vec<Sample> {
+        &self.samples
+    }
+}
+
+pub struct Reporter {}
+
+impl Reporter {
+    pub fn new() -> Self {
+        Reporter {}
+    }
+
+    pub fn report(&self, samples: &Vec<Sample>) {
+        for s in samples {
+            let ns = s.dur.as_secs() * 1_000_000_000 + (s.dur.subsec_nanos() as u64);
+            let ns_per_iter = (ns as f64) / (s.count as f64);
+            println!("{:}:\t{:?}", s.name, ns_per_iter);
+        }
+    }
+}
