79119c2bb36b6a500245474370ef533cadf6f275
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-06-24 11:54:31 -0700

    Add copyright, license info for `black_box`

--- src/black_box.rs
+++ src/black_box.rs
@@ -1,3 +1,18 @@
+// Copyright 2012-2017 The Rust Project Developers. See the COPYRIGHT
+// file at https://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the the MIT license <LICENSE-MIT or
+// https://opensource.org/licenses/MIT>. This file may not be copied,
+// modified, or distributed except according to those terms.
+//
+// The `asm!`-enabled implementation of `black_box` comes from the
+// upstream `libtest` crate by The Rust Project Developers. The
+// non-`asm!` implementation is by bluss, and comes from the `bencher`
+// crate, which is port of `libtest` to stable Rust. Both are
+// dual-licensed, and in each case we incorporate the relevant code
+// using the MIT License option.
+
+
 /// A function that is opaque to the optimizer, to allow benchmarks to
 /// pretend to use outputs to assist in avoiding dead-code elimination.
 ///
