a75d908dee3610afe909258af769fa29e9e787ea
Joe Ranweiler <joe@lemma.co>
2018-04-03 13:48:42 -0700

    Document timers

--- src/timer/mod.rs
+++ src/timer/mod.rs
@@ -1,6 +1,15 @@
+//! User-configurable timing measurement.
+
+/// Can be used to mark two points in time, and from two points, compute a
+/// spanned duration.
 pub trait Timer {
+    /// Construct a new timer.
     fn new_timer() -> Self;
+
+    /// Mark the current time.
     fn mark(&mut self);
+
+    /// Compute the time spanned by two timers.
     fn since(&self, prev: &Self) -> f64;
 }
 
